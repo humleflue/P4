d5e49f7e3a0be1bf1cf1ab70e710947b45ec1e44
Lasse Skaalum <15886122+humleflue@users.noreply.github.com>
2021-03-29 13:53:14 +0200

    Main WIP

--- ANTLR/src/Compiler/Main.java
+++ ANTLR/src/Compiler/Main.java
@@ -2,25 +2,25 @@ package Compiler;
 
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CodePointCharStream;
-import org.w3c.dom.Node;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.IOException;
 import java.io.StringReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Please enter a source program:");
         String test = System.console().readLine();
         CodePointCharStream stream = CharStreams.fromReader(new StringReader(test));
-        langLexer lexer = new langLexer(stream);
+        Lang.LangLexer lexer = new Lang.LangLexer(stream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        BParser parser = new BParser(tokens);
+        Lang.LangParser parser = new Lang.LangParser(tokens);
         try{
             ParseTree tree = parser.code();
             System.out.println("building cst");
-            Node ast = new BuildASTVisitor().visit(tree);
-            System.out.println("building ast");
-            System.out.println("done building ast");
+            System.out.println("done building cst");
         }
         catch (Exception exception){
             System.out.println("shit" + exception.getMessage());
