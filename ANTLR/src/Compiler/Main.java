a73585246e6fe79741e9b1f392ee837bad3ea8a7
Lasse Skaalum <15886122+humleflue@users.noreply.github.com>
2021-03-29 14:43:44 +0200

    Main cleanup

--- ANTLR/src/Compiler/Main.java
+++ ANTLR/src/Compiler/Main.java
@@ -1,30 +1,30 @@
 package Compiler;
 
-import Compiler.Lang.*;
+import Compiler.Lang.LangLexer;
+import Compiler.Lang.LangParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.IOException;
 import java.io.StringReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Please enter a source program:");
-        String test = System.console().readLine();
-        CodePointCharStream stream = CharStreams.fromReader(new StringReader(test));
-        LangLexer lexer = new LangLexer(stream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        LangParser parser = new LangParser(tokens);
-        try{
-            System.out.println("building cst");
+    public static void main(String[] args) {
+        try {
+            String input = "number plus(number x, number y) = return x + y; endf\nplus(1,2)?;";
+            System.out.println("Testing with program:\n```\n" + input + "\n```");
+            CodePointCharStream stream = CharStreams.fromReader(new StringReader(input));
+            LangLexer lexer = new LangLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            LangParser parser = new LangParser(tokens);
+            System.out.println("Building CST...");
             ParseTree tree = parser.code();
-            System.out.println("done building cst");
+            System.out.println("Done building CST.");
         }
         catch (Exception exception){
-            System.out.println("shit" + exception.getMessage());
+            System.out.println(exception.getStackTrace());
         }
     }
 }
