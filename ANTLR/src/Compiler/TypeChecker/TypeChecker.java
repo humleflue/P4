b8b8dbb46821c201c0aacd32b5e4582348e40193
Lasse Skaalum <lskaal19@student.auu.dk>
2021-04-02 19:11:03 +0200

    Added listener classes both custom and from antlr

---
+++ ANTLR/src/Compiler/TypeChecker/TypeChecker.java
@@ -0,0 +1,8 @@
+package Compiler.TypeChecker;
+
+import Compiler.AntlrGenerated.LangBaseVisitor;
+import Compiler.AntlrGenerated.LangParser;
+
+public class TypeChecker extends LangBaseVisitor<Integer> {
+
+}
