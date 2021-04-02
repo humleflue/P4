4f56f561948d36f80c42352b5eb60177f0708de4
Laurits <laurits.mumberg@gmail.com>
2021-04-02 09:49:54 +0200

    CheckSymbols removed. Use Main now

--- ANTLR/src/Compiler/Main.java
+++ ANTLR/src/Compiler/Main.java
@@ -2,29 +2,39 @@ package Compiler;
 
 import Compiler.Lang.LangLexer;
 import Compiler.Lang.LangParser;
+import Compiler.SymbolTable.SymbolDefListener;
+import Compiler.SymbolTable.SymbolRefListener;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-
-import java.io.StringReader;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            String input = "number plus(number x, number y) = return x + y; endf\nplus(1,2)?;";
-            System.out.println("Testing with program:\n```\n" + input + "\n```");
-            CodePointCharStream stream = CharStreams.fromReader(new StringReader(input));
+            String input = "number plus(number x, number y) = return x + z; endf\nplus(1,2)?;";
+
+            CharStream stream = CharStreams.fromString(input);
             LangLexer lexer = new LangLexer(stream);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             LangParser parser = new LangParser(tokens);
-            System.out.println("Building CST...");
-            ParseTree tree = parser.code();
-            System.out.println("Done building CST.");
+
+            ParseTree tree = parser.prog();
+            System.out.println("building cst");
+            System.out.println(tree.getText());
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+
+            SymbolDefListener symbolDefListener = new SymbolDefListener();
+            walker.walk(symbolDefListener, tree);
+
+            SymbolRefListener symbolRefListener = new SymbolRefListener(symbolDefListener.globalScope, symbolDefListener.scopes);
+            walker.walk(symbolRefListener, tree);
         }
         catch (Exception exception){
-            System.out.println(exception.getStackTrace());
+            System.out.println("ERROR: " + exception.getMessage());
         }
     }
 }
