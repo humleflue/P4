41f45392e40575f7af2b9662bcdc81e66420fbb5
Jacob Toftgaard Rasmussen <1jatora@gmail.com>
2021-05-10 10:56:28 +0200

    Added new test to parser and changed to use assertThrow instead of fail()

--- ANTLR/test/tests/LexerAndParser/LexerTests.java
+++ ANTLR/test/tests/LexerAndParser/LexerTests.java
@@ -4,68 +4,43 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Assertions;
 
 public class LexerTests extends BaseTest {
     @ParameterizedTest
     @CsvFileSource(resources = testPath + "invalidDeclarationTests.csv")
-    void invalidDeclarationsInSourceCode_ShouldThrow(String sourceCode) throws Exception {
-        assertThrows(Exception.class, () -> {
-           int errors = getNumberOfSyntaxErrors(sourceCode);
-           if (errors > 0){
-               throw new Exception();
-           }
+    void invalidDeclarationsInSourceCode_ShouldResultInErrors(String sourceCode) throws Exception {
+        Assertions.assertThrows(Exception.class, () -> {
+           getNumberOfSyntaxErrors(sourceCode);
         });
-        
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = testPath + "validDeclarationTests.csv")
     void validDeclarationsInSourceCode_ShouldPass(String sourceCode) throws IOException {
-        //arrange
         int errors = getNumberOfSyntaxErrors(sourceCode);
-        //act
-        //assert
-        if (errors > 0){
-            fail();
-        }
+        Assertions.assertEquals(0, errors);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = testPath + "expressionsTest.csv")
     void expressionsInSourceCode_ShouldPass(String sourceCode) throws IOException {
-        //arrange
         int errors = getNumberOfSyntaxErrors(sourceCode);
-        //act
-        //assert
-        if (errors > 0){
-            fail();
-        }
+        Assertions.assertEquals(0, errors);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = testPath + "ifChainTests.csv")
     void ifChainInSourceCode_ShouldPass(String sourceCode) throws IOException {
-        //arrange
         int errors = getNumberOfSyntaxErrors(sourceCode);
-        //act
-        //assert
-        if (errors > 0){
-            fail();
-        }
+        Assertions.assertEquals(0, errors);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = testPath + "functionsTest.csv")
     void functionCallsInSourceCode_ShouldPass(String sourceCode) throws IOException {
-        //arrange
         int errors = getNumberOfSyntaxErrors(sourceCode);
-        //act
-        //assert
-        if (errors > 0){
-            fail();
-        }
+        Assertions.assertEquals(0, errors);
     }
     
 }
