ef54e8b26b718f9efaa9d3e176879995f2f69e3f
christopher-jensen <cjen19@student.aau.dk>
2021-05-12 10:48:22 +0200

    added comment

--- ANTLR/src/Compiler/CodeGeneration/JavaScriptCodeGenerationVisitor.java
+++ ANTLR/src/Compiler/CodeGeneration/JavaScriptCodeGenerationVisitor.java
@@ -206,7 +206,7 @@ public class JavaScriptCodeGenerationVisitor extends BuffBaseVisitor<String> {
         if (!exprParams.isEmpty()) {
             String[] exprParamsArray = exprParams.split(",");
 
-            // Adds result of the parameter to the string
+            // Adds result of the parameters to the string
             result += getStringFromTokenList(i -> String.format("${%s},", exprParamsArray[i]),
                     exprParamsArray.length - 1);
 
