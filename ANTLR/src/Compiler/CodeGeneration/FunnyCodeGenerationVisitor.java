50445496ee8caae950b904bd6d730de35ca2ba54
Laurits <laurits.mumberg@gmail.com>
2021-05-23 19:02:21 +0200

    IntelliJ told me to do it

--- ANTLR/src/Compiler/CodeGeneration/FunnyCodeGenerationVisitor.java
+++ ANTLR/src/Compiler/CodeGeneration/FunnyCodeGenerationVisitor.java
@@ -12,19 +12,19 @@ import static Compiler.AntlrGenerated.BuffLexer.BOOLTYPE;
 
 public class FunnyCodeGenerationVisitor extends BuffBaseVisitor<ReturnValue> {
     public Map<String, FuncDefContext> functionDefinitions;
-    private Map<String, ReturnValue> parameters;
+    private Map<String, ReturnValue<?>> parameters;
 
     public FunnyCodeGenerationVisitor(Map<String, FuncDefContext> functionDefinitions) {
         this.functionDefinitions = functionDefinitions;
     }
 
-    public FunnyCodeGenerationVisitor(Map<String, FuncDefContext> functionDefinitions, Map<String, ReturnValue> parameters) {
+    public FunnyCodeGenerationVisitor(Map<String, FuncDefContext> functionDefinitions, Map<String, ReturnValue<?>> parameters) {
         this.functionDefinitions = functionDefinitions;
         this.parameters = parameters;
     }
 
     @Override
-    public ReturnValue visitProg(ProgContext ctx) {
+    public ReturnValue<?> visitProg(ProgContext ctx) {
         List<CodeContext> codeContexts = ctx.code();
         for (CodeContext code : codeContexts) {
             if(code.getRuleContext().getClass() == CodeStmtContext.class)
@@ -35,12 +35,12 @@ public class FunnyCodeGenerationVisitor extends BuffBaseVisitor<ReturnValue> {
     }
 
     @Override
-    public ReturnValue visitOneLineFunction(OneLineFunctionContext ctx) {
+    public ReturnValue<?> visitOneLineFunction(OneLineFunctionContext ctx) {
         return visit(ctx.stmt());
     }
 
     @Override
-    public ReturnValue visitMultiLineFunction(MultiLineFunctionContext ctx) {
+    public ReturnValue<?> visitMultiLineFunction(MultiLineFunctionContext ctx) {
         List<StmtsContext> stmtsContextList = ctx.stmts();
         // Evaluates whether or not the if-statement for a stmts is true, and returns its expression if it is.
         for(StmtsContext stmts : stmtsContextList){
@@ -53,18 +53,18 @@ public class FunnyCodeGenerationVisitor extends BuffBaseVisitor<ReturnValue> {
     }
 
     @Override
-    public ReturnValue visitStmt(StmtContext ctx) {
+    public ReturnValue<?> visitStmt(StmtContext ctx) {
         return visit(ctx.expr());
     }
 
     @Override
-    public ReturnValue visitExprParenthesised(ExprParenthesisedContext ctx) {
+    public ReturnValue<?> visitExprParenthesised(ExprParenthesisedContext ctx) {
         return visit(ctx.expr());
     }
 
     @Override
-    public ReturnValue visitFuncCall(FuncCallContext ctx) {
-        Map<String, ReturnValue> actualArguments = new LinkedHashMap<>();
+    public ReturnValue<?> visitFuncCall(FuncCallContext ctx) {
+        Map<String, ReturnValue<?>> actualArguments = new LinkedHashMap<>();
 
         if(ctx.exprParams() != null){
             actualArguments = makeArgumentsMapForFuncCall(ctx.ID().getText(), ctx.exprParams());
@@ -74,8 +74,14 @@ public class FunnyCodeGenerationVisitor extends BuffBaseVisitor<ReturnValue> {
         return funcCallEvaluator.visit(functionDefinitions.get(ctx.ID().getText()));
     }
 
-    private Map<String, ReturnValue> makeArgumentsMapForFuncCall(String functionName, ExprParamsContext exprParams){
-        Map<String, ReturnValue> actualArguments = new LinkedHashMap<>();
+    /**
+     * Creates the map between parameter names and their actual values that is needed by this visitor
+     * @param functionName The name of the function being called
+     * @param exprParams The ExprParams node from the FuncCall. Used to get the values
+     * @return Returns a map between parameter names and ReturnValues
+     */
+    private Map<String, ReturnValue<?>> makeArgumentsMapForFuncCall(String functionName, ExprParamsContext exprParams){
+        Map<String, ReturnValue<?>> actualArguments = new LinkedHashMap<>();
 
         int exprCount = exprParams.expr().size();
         for (int i = 0; i < exprCount; i++) {
@@ -92,66 +98,66 @@ public class FunnyCodeGenerationVisitor extends BuffBaseVisitor<ReturnValue> {
     }
 
     @Override
-    public ReturnValue visitExprFunccallPrint(ExprFunccallPrintContext ctx) {
-        ReturnValue returnValue = visit(ctx.funcCall());
+    public ReturnValue<?> visitExprFunccallPrint(ExprFunccallPrintContext ctx) {
+        ReturnValue<?> returnValue = visit(ctx.funcCall());
         System.out.println(ctx.funcCall().getText() + " => " + returnValue.value);
         return returnValue;
     }
 
     @Override
-    public ReturnValue visitExprNumber(ExprNumberContext ctx) {
+    public ReturnValue<?> visitExprNumber(ExprNumberContext ctx) {
         double value = Double.parseDouble(ctx.NUMLITERAL().getText());
         return new ReturnValue<Double>(value, NUMTYPE);
     }
 
     @Override
-    public ReturnValue visitExprBoolean(ExprBooleanContext ctx) {
+    public ReturnValue<?> visitExprBoolean(ExprBooleanContext ctx) {
         boolean value = Boolean.parseBoolean(ctx.BOOLLITERAL().getText());
         return new ReturnValue<Boolean>(value, BOOLTYPE);
     }
 
     @Override
-    public ReturnValue visitExprId(ExprIdContext ctx) {
+    public ReturnValue<?> visitExprId(ExprIdContext ctx) {
         return parameters.get(ctx.ID().getText());
     }
 
     @Override
-    public ReturnValue visitExprMinusPrefix(ExprMinusPrefixContext ctx) {
+    public ReturnValue<?> visitExprMinusPrefix(ExprMinusPrefixContext ctx) {
         ReturnValue<Double> returnValue = visit(ctx.expr());
         returnValue.value = -returnValue.value;
         return returnValue;
     }
 
     @Override
-    public ReturnValue visitExprUnaryOp(ExprUnaryOpContext ctx) {
+    public ReturnValue<?> visitExprUnaryOp(ExprUnaryOpContext ctx) {
         ReturnValue<Boolean> returnValue = visit(ctx.expr());
-        returnValue.value = returnValue.value ? false : true;
+        returnValue.value = !returnValue.value;
         return returnValue;
     }
 
     @Override
-    public ReturnValue visitExprBinaryOp(ExprBinaryOpContext ctx) {
+    public ReturnValue<?> visitExprBinaryOp(ExprBinaryOpContext ctx) {
         return switch (ctx.op.getType()) {
-            case DIVIDE -> new ReturnValue<Double>((Double)visit(ctx.left).value / (Double)visit(ctx.right).value, NUMTYPE);
-            case MULTIPLY -> new ReturnValue<Double>((Double)visit(ctx.left).value * (Double)visit(ctx.right).value, NUMTYPE);
-            case PLUS -> new ReturnValue<Double>((Double)visit(ctx.left).value + (Double)visit(ctx.right).value, NUMTYPE);
-            case MINUS -> new ReturnValue<Double>((Double)visit(ctx.left).value - (Double)visit(ctx.right).value, NUMTYPE);
-            case LOGLESS -> new ReturnValue<Boolean>((Double)visit(ctx.left).value < (Double) visit(ctx.right).value, BOOLTYPE);
-            case LOGGREATER -> new ReturnValue<Boolean>((Double)visit(ctx.left).value > (Double) visit(ctx.right).value, BOOLTYPE);
-            case LOGLESSOREQ -> new ReturnValue<Boolean>((Double)visit(ctx.left).value <= (Double) visit(ctx.right).value, BOOLTYPE);
-            case LOGGREATEROREQ -> new ReturnValue<Boolean>((Double)visit(ctx.left).value >= (Double) visit(ctx.right).value, BOOLTYPE);
-            case LOGEQ -> new ReturnValue<Boolean>(visit(ctx.left).value.equals(visit(ctx.right).value), BOOLTYPE);
-            case LOGNOTEQ -> new ReturnValue<Boolean>(!visit(ctx.left).value.equals(visit(ctx.right).value), BOOLTYPE);
-            case LOGAND -> new ReturnValue<Boolean>((Boolean) visit(ctx.left).value && (Boolean) visit(ctx.right).value, BOOLTYPE);
-            case LOGOR -> new ReturnValue<Boolean>((Boolean) visit(ctx.left).value || (Boolean) visit(ctx.right).value, BOOLTYPE);
+            case DIVIDE -> new ReturnValue<>((Double)visit(ctx.left).value / (Double)visit(ctx.right).value, NUMTYPE);
+            case MULTIPLY -> new ReturnValue<>((Double)visit(ctx.left).value * (Double)visit(ctx.right).value, NUMTYPE);
+            case PLUS -> new ReturnValue<>((Double)visit(ctx.left).value + (Double)visit(ctx.right).value, NUMTYPE);
+            case MINUS -> new ReturnValue<>((Double)visit(ctx.left).value - (Double)visit(ctx.right).value, NUMTYPE);
+            case LOGLESS -> new ReturnValue<>((Double)visit(ctx.left).value < (Double) visit(ctx.right).value, BOOLTYPE);
+            case LOGGREATER -> new ReturnValue<>((Double)visit(ctx.left).value > (Double) visit(ctx.right).value, BOOLTYPE);
+            case LOGLESSOREQ -> new ReturnValue<>((Double)visit(ctx.left).value <= (Double) visit(ctx.right).value, BOOLTYPE);
+            case LOGGREATEROREQ -> new ReturnValue<>((Double)visit(ctx.left).value >= (Double) visit(ctx.right).value, BOOLTYPE);
+            case LOGEQ -> new ReturnValue<>(visit(ctx.left).value.equals(visit(ctx.right).value), BOOLTYPE);
+            case LOGNOTEQ -> new ReturnValue<>(!visit(ctx.left).value.equals(visit(ctx.right).value), BOOLTYPE);
+            case LOGAND -> new ReturnValue<>((Boolean) visit(ctx.left).value && (Boolean) visit(ctx.right).value, BOOLTYPE);
+            case LOGOR -> new ReturnValue<>((Boolean) visit(ctx.left).value || (Boolean) visit(ctx.right).value, BOOLTYPE);
             default -> throw new IllegalArgumentException(
                     "Could not recognize " + ctx.op.getText() + "as a binary operator.");
         };
     }
 
     @Override
-    public ReturnValue visitExprPrint(ExprPrintContext ctx) {
-        ReturnValue returnValue = visit(ctx.expr());
+    public ReturnValue<?> visitExprPrint(ExprPrintContext ctx) {
+        ReturnValue<?> returnValue = visit(ctx.expr());
         System.out.println(ctx.expr().getText() + " => " + returnValue.value);
         return returnValue;
     }
